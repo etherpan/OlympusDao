259cd9f029ab8552ce8a798b319c2f6e023b48ee
jovoel <kryptoexpert1@gmail.com>
2023-08-02 04:11:41 +1000

    initial config

--- vite.config.js
+++ vite.config.js
@@ -26,16 +26,6 @@ export default ({ mode }) => {
     },
     build: {
       outDir: "./build",
-      chunkSizeWarningLimit: 1600,
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes("node_modules")) {
-              return id.toString().split("node_modules/")[1].split("/")[0].toString();
-            }
-          },
-        },
-      },
     },
     test: {
       setupFiles: "src/setupTests.tsx",
